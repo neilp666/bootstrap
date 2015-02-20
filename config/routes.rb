8b41bce018456520b466fde665ebdd486edf9bcc
neil patel <neil@myindianweddingapp.com>
2015-02-20 10:10:08 +0000

    added js/bootstrap & images

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
-  get 'pages/about'
+  get 'about' => 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
